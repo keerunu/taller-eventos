546fbb4d135ff6ad9946a0ae4589fec1954b52aa
keerunu <nacho_rodao13@hotmail.com>
2023-08-06 17:03:04 -0300

    Solucionado propagación

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 const div = document.querySelector("div");
 
+function saludoBtn (event) {
+    event.stopPropagation();
+    alert("Hola!");
+}
+
 div.addEventListener("click", () => {alert("Hola! Soy el div")});
